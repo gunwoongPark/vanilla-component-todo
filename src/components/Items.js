8e086a2213d718ec9b57d3c5aa088df559f721a1
gunwoongPark <bung1438@gmail.com>
2022-12-05 19:53:15 +0900

    feat: 이벤트 추가 로직 추상화

--- src/components/Items.js
+++ src/components/Items.js
@@ -25,17 +25,14 @@ export default class Items extends Component {
   }
 
   setEvent() {
-    this.$target.querySelector(".addBtn").addEventListener("click", () => {
+    this.addEvent("click", ".addBtn", () => {
       const { items } = this.$state;
       this.setState({ items: [...items, `item${items.length + 1}`] });
     });
-
-    this.$target.querySelectorAll(".deleteBtn").forEach((deleteBtn) =>
-      deleteBtn.addEventListener("click", ({ target }) => {
-        const items = [...this.$state.items];
-        items.splice(target.dataset.index, 1);
-        this.setState({ items });
-      })
-    );
+    this.addEvent("click", ".deleteBtn", ({ target }) => {
+      const items = [...this.$state.items];
+      items.splice(target.dataset.index, 1);
+      this.setState({ items });
+    });
   }
 }
--- src/core/Component.js
+++ src/core/Component.js
@@ -5,6 +5,7 @@ export default class Component {
   constructor($target) {
     this.$target = $target;
     this.setup();
+    this.setEvent();
     this.render();
   }
 
@@ -16,7 +17,6 @@ export default class Component {
 
   render() {
     this.$target.innerHTML = this.template();
-    this.setEvent();
   }
 
   setEvent() {}
@@ -24,5 +24,17 @@ export default class Component {
   setState(newState) {
     this.$state = { ...this.$state, ...newState };
     this.render();
+  }
+
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+
+    const isTarget = (target) =>
+      children.includes(target) || target.closest(selector);
+
+    this.$target.addEventListener(eventType, (e) => {
+      if (!isTarget(e.target)) return false;
+      callback(e);
+    });
   }
 }
